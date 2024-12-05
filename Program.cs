035c1542e8f1f59f608ead179b3b6e36b24ed39c
Gregoire Bouteillier <gregoire.bouteillier@wedafr.onmicrosoft.com>
2024-12-05 15:01:44 +0100

    Quick solution for part 2

--- Program.cs
+++ Program.cs
@@ -9,17 +9,47 @@ namespace AdventOfCode2024Day3
             string path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "data", "input.txt");
 
             int result = 0;
+            int result2 = 0;
 
             // parcourir le fichier avec un streamreader
             using (StreamReader sr = new StreamReader(path))
             {
                 // repérer uniquement les éléments qui ont ce format là "mul(xxx1,xxx2)"
                 string regexPattern = @"mul\(\d{1,3},\d{1,3}\)";
+                string secondRegexPattern = @"mul\(\d{1,3},\d{1,3}\)|do\(\)|don't\(\)";
 
                 Regex regex = new Regex(regexPattern);
+                Regex secondRegex = new Regex(secondRegexPattern);
+               
+                string content = sr.ReadToEnd();
 
-                MatchCollection match = regex.Matches(sr.ReadToEnd());
+                MatchCollection match = regex.Matches(content);
+                MatchCollection match2 = secondRegex.Matches(content);
                 string[] numbers;
+
+                bool isDo = true;
+
+                foreach (Match m in match2)
+                {
+                    if (m.Value == "do()")
+                    {
+                        isDo = true;
+                    }
+
+                    if (m.Value == "don't()")
+                    {
+                        isDo = false;
+                        continue;
+                    }
+
+                    if (m.Value.Contains("mul") && isDo)
+                    {
+                        string value = m.ToString().Remove(m.Length - 1).Remove(0, 4);
+                        numbers = value.Split(',').ToArray();
+                        int multiply = Int32.Parse(numbers[0]) * Int32.Parse(numbers[1]);
+                        result2 += multiply;
+                    }
+                }
 
                 foreach (Match m in match)
                 {
@@ -32,9 +62,10 @@ namespace AdventOfCode2024Day3
                     result += multiply;
                 }
             }
-
-            // retourner la variable finale.
+            //retourner la variable finale.
             Console.WriteLine(result.ToString());
+
+            Console.Write(result2.ToString());
         }
     }
 }
--- obj/Debug/net8.0/AdventOfCode2024Day3.AssemblyInfo.cs
+++ obj/Debug/net8.0/AdventOfCode2024Day3.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AdventOfCode2024Day3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+490e7e22849cbcb2b36edcdf6c0e11baad89a6ac")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5fd49011658d3fa9107f8942e0ec8cd621869e26")]
 [assembly: System.Reflection.AssemblyProductAttribute("AdventOfCode2024Day3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AdventOfCode2024Day3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
