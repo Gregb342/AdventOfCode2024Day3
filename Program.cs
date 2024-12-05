95d8efbf6dcb4f94f9789385736e1a6958bee2e2
Gregoire Bouteillier <gregoire.bouteillier@wedafr.onmicrosoft.com>
2024-12-05 12:20:32 +0100

    First Star quick solution

--- Program.cs
+++ Program.cs
@@ -1,10 +1,41 @@
-﻿namespace AdventOfCode2024Day3
+﻿using System.Text.RegularExpressions;
+
+namespace AdventOfCode2024Day3
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "data", "input.txt");
+
+            int result = 0;       
+            
+            // retourner la variable finale.
+
+            // parcourir le fichier avec un streamreader
+            StreamReader sr = new StreamReader(path);
+
+            // repérer uniquement les éléments qui ont ce format là "mul(xxx1,xxx2)"
+            string regexPattern = @"mul\(\d{1,3},\d{1,3}\)";
+
+            Regex regex = new Regex(regexPattern);
+
+            MatchCollection match = regex.Matches(sr.ReadToEnd());
+            string[] numbers;
+
+            foreach (Match m in match)
+            {
+                // pour chaque élément repéré, calcul xxx1 * xxx2
+                string value = m.ToString().Remove(m.Length - 1).Remove(0, 4);
+                numbers = value.Split(',').ToArray();
+
+                int multiply = Int32.Parse(numbers[0]) * Int32.Parse(numbers[1]);
+                // ajouter le résultat à une variable finale
+                result += multiply;
+                Array.Clear(numbers);
+            }
+
+            Console.WriteLine(result.ToString());
         }
     }
 }
--- obj/Debug/net8.0/AdventOfCode2024Day3.AssemblyInfo.cs
+++ obj/Debug/net8.0/AdventOfCode2024Day3.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AdventOfCode2024Day3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f6f2e832e9d0cd50f1d89758be5752d1ec556ad7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+490e7e22849cbcb2b36edcdf6c0e11baad89a6ac")]
 [assembly: System.Reflection.AssemblyProductAttribute("AdventOfCode2024Day3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AdventOfCode2024Day3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
