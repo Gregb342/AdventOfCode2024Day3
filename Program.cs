5fd49011658d3fa9107f8942e0ec8cd621869e26
Gregoire Bouteillier <gregoire.bouteillier@wedafr.onmicrosoft.com>
2024-12-05 13:33:08 +0100

    Minor Improvement of actual code.

--- Program.cs
+++ Program.cs
@@ -8,33 +8,32 @@ namespace AdventOfCode2024Day3
         {
             string path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "data", "input.txt");
 
-            int result = 0;       
-            
-            // retourner la variable finale.
+            int result = 0;
 
             // parcourir le fichier avec un streamreader
-            StreamReader sr = new StreamReader(path);
-
-            // repérer uniquement les éléments qui ont ce format là "mul(xxx1,xxx2)"
-            string regexPattern = @"mul\(\d{1,3},\d{1,3}\)";
-
-            Regex regex = new Regex(regexPattern);
-
-            MatchCollection match = regex.Matches(sr.ReadToEnd());
-            string[] numbers;
-
-            foreach (Match m in match)
+            using (StreamReader sr = new StreamReader(path))
             {
-                // pour chaque élément repéré, calcul xxx1 * xxx2
-                string value = m.ToString().Remove(m.Length - 1).Remove(0, 4);
-                numbers = value.Split(',').ToArray();
+                // repérer uniquement les éléments qui ont ce format là "mul(xxx1,xxx2)"
+                string regexPattern = @"mul\(\d{1,3},\d{1,3}\)";
 
-                int multiply = Int32.Parse(numbers[0]) * Int32.Parse(numbers[1]);
-                // ajouter le résultat à une variable finale
-                result += multiply;
-                Array.Clear(numbers);
+                Regex regex = new Regex(regexPattern);
+
+                MatchCollection match = regex.Matches(sr.ReadToEnd());
+                string[] numbers;
+
+                foreach (Match m in match)
+                {
+                    // pour chaque élément repéré, calcul xxx1 * xxx2
+                    string value = m.ToString().Remove(m.Length - 1).Remove(0, 4);
+                    numbers = value.Split(',').ToArray();
+
+                    int multiply = Int32.Parse(numbers[0]) * Int32.Parse(numbers[1]);
+                    // ajouter le résultat à une variable finale
+                    result += multiply;
+                }
             }
 
+            // retourner la variable finale.
             Console.WriteLine(result.ToString());
         }
     }
